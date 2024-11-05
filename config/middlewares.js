e795e7c2ff5238c0f4ae3f490e0628ea90494935
SergWebAcceptance <serg.web@acceptance.ltd>
2024-11-05 14:48:19 +0200

    Fixed plugins

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,8 +1,29 @@
 module.exports = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['http://localhost:3000', 'https://velloxia.com'], // Replace with your frontend URLs
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization'],
+      credentials: true, // Include if you need to send cookies with requests
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,25 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+    "users-permissions": {
+      config: {
+        register: {
+          // put the name of your added fields here
+          allowedFields: ["firstName", "lastName", "phone"],
+        },
+      },
+    },
+    upload: {
+      config: {
+        provider: 'cloudinary',
+        providerOptions: {
+          cloud_name: env('CLOUDINARY_NAME'),
+          api_key: env('CLOUDINARY_KEY'),
+          api_secret: env('CLOUDINARY_SECRET'),
+        },
+        actionOptions: {
+          upload: {},
+          delete: {},
+        },
+      },
+    },
+  });
+  
