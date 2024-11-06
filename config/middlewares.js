5b473700c3c4865824e8287700f6a5c34f4230aa
SergWebAcceptance <serg.web@acceptance.ltd>
2024-11-06 11:57:06 +0200

    fixed holaxgroup

--- config/middlewares.js
+++ config/middlewares.js
@@ -18,7 +18,7 @@ module.exports = [
   {
     name: 'strapi::cors',
     config: {
-      origin: ['http://localhost:3000', 'https://velloxia.com'], // Replace with your frontend URLs
+      origin: ['http://localhost:3000', 'https://holaxgroup.com'], // Replace with your frontend URLs
       methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
       headers: ['Content-Type', 'Authorization'],
       credentials: true, // Include if you need to send cookies with requests
