6f6217ee82f489511daee30cc0979e427fa0fd4d
SergWebAcceptance <serg.web@acceptance.ltd>
2024-11-08 14:22:04 +0200

    added nodemailer

--- config/plugins.js
+++ config/plugins.js
@@ -1,25 +1,45 @@
 module.exports = ({ env }) => ({
-    "users-permissions": {
-      config: {
-        register: {
-          // put the name of your added fields here
-          allowedFields: ["firstName", "lastName", "phone"],
-        },
+  "users-permissions": {
+    config: {
+      register: {
+        // put the name of your added fields here
+        allowedFields: ["firstName", "lastName", "phone"],
       },
     },
-    upload: {
-      config: {
-        provider: 'cloudinary',
-        providerOptions: {
-          cloud_name: env('CLOUDINARY_NAME'),
-          api_key: env('CLOUDINARY_KEY'),
-          api_secret: env('CLOUDINARY_SECRET'),
+  },
+  email: {
+    config: {
+      provider: "nodemailer",
+      providerOptions: {
+        host: env("SMTP_HOST", "smtp.gmail.com"),
+        port: env.int("SMTP_PORT", 587), // Ensure the port is an integer
+        auth: {
+          user: env("SMTP_USERNAME"),
+          pass: env("SMTP_PASSWORD"),
         },
-        actionOptions: {
-          upload: {},
-          delete: {},
+        secure: env.bool("SMTP_SECURE", false), // Parse the secure option as a boolean
+        tls: {
+          rejectUnauthorized: false, // Disable strict SSL checking
         },
       },
+      settings: {
+        defaultFrom: "noreply@holaxgroup.com ",
+        defaultReplyTo: "noreply@holaxgroup.com ",
+      },
     },
-  });
-  
+  },
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
+    },
+  },
+});
